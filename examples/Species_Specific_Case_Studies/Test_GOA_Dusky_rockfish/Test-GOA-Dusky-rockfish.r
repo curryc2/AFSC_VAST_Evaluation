1723e5bc28d434e0443d0a74d3653246aef2710f
Curry Cunningham <cunninghamcurry@gmail.com>
2017-10-11 08:31:52 -0800

    Updated dusky rockfish example

--- examples/Species_Specific_Case_Studies/Test_GOA_Dusky_rockfish/Test-GOA-Dusky-rockfish.r
+++ examples/Species_Specific_Case_Studies/Test_GOA_Dusky_rockfish/Test-GOA-Dusky-rockfish.r
@@ -189,40 +189,40 @@ SpatialDeltaGLMM::PlotResultsOnMap_Fn(plot_set = c(3),
                                       Legend = MapDetails_List[["Legend"]], zone = MapDetails_List[["Zone"]],
                                       mar = c(0, 0, 2, 0), oma = c(3.5, 3.5, 0, 0), cex = 1.8,
                                       plot_legend_fig = TRUE)
-# 
-# 
-# 
-# #Generate Index of Abundance
-# 
-# Index = SpatialDeltaGLMM::PlotIndex_Fn(DirName = DateFile,
-#                                        TmbData = TmbData, Sdreport = Opt[["SD"]],
-#                                        Year_Set = Year_Set,
-#                                        Years2Include = Years2Include, 
-#                                        use_biascorr = TRUE)
-# 
-# idx <- Index$Table
-# 
-# 
-# dev.off()
-# #Plotting 
-# 
-# yrs.surv <- Year_Set[Years2Include]
-# x.lim <- c(min(yrs.surv), max(yrs.surv))
-# up.sd <- idx$Estimate_metric_tons + idx$SD_mt
-# low.sd <- idx$Estimate_metric_tons - idx$SD_mt
-# y.lim <- c(min(low.sd), max(up.sd))
-# 
-# loc.yrs <- which(idx$Year %in% yrs.surv)
-# 
-# 
-# plot(x=NULL, y=NULL, xlim=x.lim, ylim=y.lim, ylab='Survey Estimate (metric Tons)', xlab='Year',
-#      main='Gulf of Alaska\nNorthern Rockfish Survey Index')
-# 
-# polygon(x=c(yrs.surv, rev(yrs.surv)), y=c(low.sd[loc.yrs],rev(up.sd[loc.yrs])), col='lightblue', border=FALSE)
-# lines(x=yrs.surv, y=idx$Estimate_metric_tons[loc.yrs], col='red')
-# points(x=yrs.surv, y=idx$Estimate_metric_tons[loc.yrs], pch=21, bg='red')
-# grid(col='black')
-# 
+
+
+
+#Generate Index of Abundance
+
+Index = SpatialDeltaGLMM::PlotIndex_Fn(DirName = DateFile,
+                                       TmbData = TmbData, Sdreport = Opt[["SD"]],
+                                       Year_Set = Year_Set,
+                                       Years2Include = Years2Include,
+                                       use_biascorr = TRUE)
+
+idx <- Index$Table
+
+
+dev.off()
+#Plotting
+
+yrs.surv <- Year_Set[Years2Include]
+x.lim <- c(min(yrs.surv), max(yrs.surv))
+up.sd <- idx$Estimate_metric_tons + idx$SD_mt
+low.sd <- idx$Estimate_metric_tons - idx$SD_mt
+y.lim <- c(min(low.sd), max(up.sd))
+
+loc.yrs <- which(idx$Year %in% yrs.surv)
+
+
+plot(x=NULL, y=NULL, xlim=x.lim, ylim=y.lim, ylab='Survey Estimate (metric Tons)', xlab='Year',
+     main='Gulf of Alaska\nNorthern Rockfish Survey Index')
+
+polygon(x=c(yrs.surv, rev(yrs.surv)), y=c(low.sd[loc.yrs],rev(up.sd[loc.yrs])), col='lightblue', border=FALSE)
+lines(x=yrs.surv, y=idx$Estimate_metric_tons[loc.yrs], col='red')
+points(x=yrs.surv, y=idx$Estimate_metric_tons[loc.yrs], pch=21, bg='red')
+grid(col='black')
+
 # 
 # #Center of gravity and range expansion/contraction
 # #  For some reason I can't actually change the years to plot 
